39f23ca04031ef04aef8a01871bf14921153729d
azizamistral <Aziza@Aziza-Mistrals-MacBook-Air.local>
2016-07-15 16:24:19 -0700

    Updated To-Do list

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,10 +1,8 @@
 var form = document.querySelector("#new-item-form");
 var ul = document.querySelector("#todo-list");
-var input = document.querySelector("#new-item-input")
+var input = document.querySelector("#new-item-input");
 
-function formSubmitted(event) {
-  event.preventDefault();
-
+function newItem(groceryThing){
   var li = document.createElement("li");
   var span = document.createElement("span");
   var checkbox = document.createElement("input");
@@ -12,11 +10,22 @@ function formSubmitted(event) {
 
   ul.appendChild(li).appendChild(label);
   label.appendChild(checkbox).setAttribute("type","checkbox");
-  label.appendChild(span).textContent = input.value;
+  label.appendChild(span).textContent = groceryThing.trim( );
+}
 
-  // Your code goes here...
-  //input.value.split(",").forEach(formSubmitted);
-  // forEach(listItem)
+function formSubmitted(event) {
+  event.preventDefault();
+
+  input.value.split(",").forEach(newItem);
+
+  // var li = document.createElement("li");
+  // var span = document.createElement("span");
+  // var checkbox = document.createElement("input");
+  // var label = document.createElement("label");
+
+  // ul.appendChild(li).appendChild(label);
+  // label.appendChild(checkbox).setAttribute("type","checkbox");
+  // label.appendChild(span).textContent = input.value;
 
   form.reset();
 }
